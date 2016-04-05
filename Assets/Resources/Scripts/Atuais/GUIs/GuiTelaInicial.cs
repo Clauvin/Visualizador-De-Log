37858530ebdef02cb65b9336ca82dc07df027491
Clauvin <clauvin_almeida@hotmail.com>
2016-04-05 13:18:57 -0300

    Alteração da data na tela inicial.

--- Assets/Resources/Scripts/Atuais/GUIs/GuiTelaInicial.cs
+++ Assets/Resources/Scripts/Atuais/GUIs/GuiTelaInicial.cs
@@ -20,7 +20,7 @@ public class GuiTelaInicial : GuiPadrao2
         posicaox = 0;
         GUI.Box(new Rect(0, 0, Screen.width, Screen.height), string.Empty);
         GUI.Box(new Rect(Screen.width / 4, Screen.height / 6, Screen.width / 2, Screen.height / 2), "Visualizador de Logs\n\n" +
-            "F!T e Bolhas\n\n" + "Protótipo versão 27/03/2016", estilotitulotelainicial);
+            "F!T e Bolhas\n\n" + "Protótipo versão 05/04/2016", estilotitulotelainicial);
 
         resultado = GUI.Toolbar(new Rect(Screen.width / 12 * 3, Screen.height / 10 * 8, Screen.width / 12 * 6, Screen.height / 10), qualbotao,
             toolbarStrings);
