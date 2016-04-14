e3f861582417b653d39f14ffe590290b45d03d87
Clauvin <clauvin_almeida@hotmail.com>
2016-04-14 17:31:29 -0300

    Mudanças em CounterParser3 para legibilidade.

--- Assets/Resources/Scripts/Atuais/TestLogFITGenerator.cs
+++ Assets/Resources/Scripts/Atuais/TestLogFITGenerator.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CounterParser3 : MonoBehaviour
+public class TestLogFITGenerator : MonoBehaviour
 {
 
     ArrayList tempo;
@@ -11,8 +11,8 @@ public class CounterParser3 : MonoBehaviour
     public int inity1 = 1;
     public int initx2 = 1;
     public int inity2 = 2;
-    public int quantasvezes = 100;
-    string endereco;
+    public int quantos_objetos_criar = 100;
+    string endereco_onde_salvar;
 
     // Use this for initialization
     void Start()
@@ -20,30 +20,21 @@ public class CounterParser3 : MonoBehaviour
         tempo = new ArrayList();
         qual_objeto = new ArrayList();
         posicoes_dos_objetos = new ArrayList();
-        QualEndereco();
-        Gerador();
-        Salvador();
-
+        Onde_Salvar_Texto_De_Log();
+        Gerador_De_Dados_De_Log();
+        Salvador_De_Texto_De_Log();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-    void Gerador()
+    void Gerador_De_Dados_De_Log()
     {
         int temp;
         int x1 = initx1 * 32;
         int y1 = inity1 * 32;
         int x2 = initx2 * 32;
         int y2 = inity2 * 32;
-        int oquefoi;
-        float r1;
-        int alpha = 0;
-        int beta = 0;
-        for (int i = 0; i < quantasvezes; i++)
+        int o_que_foi;
+
+        for (int i = 0; i < quantos_objetos_criar; i++)
         {
             if ((i % 2 == 0) && (i < 21))             x1 += 32;
             if ((i % 2 == 0) && (i > 21) && (i < 38)) y1 += 32;
@@ -53,26 +44,25 @@ public class CounterParser3 : MonoBehaviour
             if ((i % 2 == 1) && (i > 72) && (i < 92)) x2 -= 32;
 
             temp = i / 2;
-            oquefoi = (i % 2) + 1;
+            o_que_foi = (i % 2) + 1;
             if (i % 2 == 0) posicoes_dos_objetos.Add(new Vector2(x1, y1));
             if (i % 2 == 1) posicoes_dos_objetos.Add(new Vector2(x2, y2));
             tempo.Add(temp);
-            qual_objeto.Add(oquefoi);
+            qual_objeto.Add(o_que_foi);
         }
 
-        for (int i = 0; i < quantasvezes; i++)
-        {
+        for (int i = 0; i < quantos_objetos_criar; i++) {
             Vector2 posicao = (Vector2)posicoes_dos_objetos[i];
-
-
         }
     }
 
-    void Salvador()
+    void Salvador_De_Texto_De_Log()
     {
-        System.IO.StreamWriter file = new System.IO.StreamWriter(endereco);
+        System.IO.StreamWriter file = new System.IO.StreamWriter(endereco_onde_salvar);
+
         file.WriteLine("[Mode 01]");
-        for (int i = 0; i < quantasvezes; i++)
+
+        for (int i = 0; i < quantos_objetos_criar; i++)
         {
             Vector2 posicao = (Vector2)posicoes_dos_objetos[i];
             if (i%2==0) file.WriteLine("TimeA:" + tempo[i] + "=Char:" + qual_objeto[i] + "=GridX:" + posicao.x + "=GridY:"
@@ -83,9 +73,8 @@ public class CounterParser3 : MonoBehaviour
         file.Close();
     }
 
-    void QualEndereco()
+    void Onde_Salvar_Texto_De_Log()
     {
-        endereco = "C:\\Teste\\Teste2.txt";
-
+        endereco_onde_salvar = "C:\\Teste\\Teste2.txt";
     }
 }
