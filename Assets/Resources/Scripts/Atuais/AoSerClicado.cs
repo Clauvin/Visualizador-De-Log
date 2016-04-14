93a43e248ff1c0861a8cbf1b62461253254c51a4
Clauvin <clauvin_almeida@hotmail.com>
2016-04-14 16:05:32 -0300

    Melhora de AoSerClicado para ser mais legível

--- Assets/Resources/Scripts/Atuais/AoSerClicado.cs
+++ Assets/Resources/Scripts/Atuais/AoSerClicado.cs
@@ -3,16 +3,6 @@ using System.Collections;
 
 public class AoSerClicado : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
     void OnMouseDown()
     {
         Dados d = GetComponent<Dados>();
