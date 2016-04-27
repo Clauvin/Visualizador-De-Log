5d8e7337706f11a233a904aa7c433d2f1dbf61ea
Clauvin <clauvin_almeida@hotmail.com>
2016-04-26 22:03:40 -0300

    Conserto de bug de herança

--- Assets/Resources/Scripts/Atuais/GUIs/GuiFITModo.cs
+++ Assets/Resources/Scripts/Atuais/GUIs/GuiFITModo.cs
@@ -3,13 +3,4 @@ using System.Collections;
 
 public class GuiFITModo : GuiModo {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }
