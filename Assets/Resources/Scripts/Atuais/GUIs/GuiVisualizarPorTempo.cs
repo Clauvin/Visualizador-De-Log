10b2e087cbcb7dc72fbc0881d0fe160a7bc6ce44
Clauvin <clauvin_almeida@hotmail.com>
2016-05-12 12:26:32 -0300

    Melhora do Tratar de Erro de GuiVisualizarPorTempo

--- Assets/Resources/Scripts/Atuais/GUIs/GuiVisualizarPorTempo.cs
+++ Assets/Resources/Scripts/Atuais/GUIs/GuiVisualizarPorTempo.cs
@@ -27,6 +27,8 @@ public class GuiVisualizarPorTempo : GuiPadrao2 {
     // é melhor deixar as deteccoes e tratamentos de input errado numa função só pra isso
     // do que espalhar elas pelo código.
     bool erro_de_input_minimo_menor_que_limite_minimo = false;
+    bool erro_de_input_minimo_maior_que_limite_maximo = false;
+    bool erro_de_input_maximo_menor_que_limite_minimo = false;
     bool erro_de_input_maximo_maior_que_limite_maximo = false;
     bool erro_de_maior_que = false;
     int posicao_erro_y = 100;
@@ -42,11 +44,13 @@ public class GuiVisualizarPorTempo : GuiPadrao2 {
 
         // esse reset das variáveis ao usar essa função é necessário para evitar
         // que as mensagens de inputs errados bloqueiem as mensagens de inputs vazios.
-        erro_de_input_errado_minimo = false;
-        erro_de_input_errado_maximo = false;
         erro_de_input_vazio_minimo = false;
         erro_de_input_vazio_maximo = false;
+        erro_de_input_errado_minimo = false;
+        erro_de_input_errado_maximo = false;
         erro_de_input_minimo_menor_que_limite_minimo = false;
+        erro_de_input_minimo_maior_que_limite_maximo = false;
+        erro_de_input_maximo_menor_que_limite_minimo = false;
         erro_de_input_maximo_maior_que_limite_maximo = false;
         erro_de_maior_que = false;
 
@@ -54,25 +58,37 @@ public class GuiVisualizarPorTempo : GuiPadrao2 {
 
         if (tempo_maximo == "") { erro_de_input_vazio_maximo = true; }
 
-        long long_minimo = long.Parse(tempo_minimo, NumberStyles.AllowThousands | NumberStyles.AllowLeadingSign);
-        long long_maximo = long.Parse(tempo_maximo, NumberStyles.AllowThousands | NumberStyles.AllowLeadingSign);
-        if (long_minimo < Int32.MinValue)
+        if(!(erro_de_input_vazio_minimo || erro_de_input_vazio_maximo))
         {
-            erro_de_input_minimo_menor_que_limite_minimo = true;
+            long long_minimo = long.Parse(tempo_minimo, NumberStyles.AllowThousands | NumberStyles.AllowLeadingSign);
+            long long_maximo = long.Parse(tempo_maximo, NumberStyles.AllowThousands | NumberStyles.AllowLeadingSign);
+            if (long_minimo < Int32.MinValue)
+            {
+                erro_de_input_minimo_menor_que_limite_minimo = true;
+            }
+            if (long_minimo > Int32.MaxValue)
+            {
+                erro_de_input_minimo_maior_que_limite_maximo = true;
+            }
+            if (long_maximo > Int32.MinValue)
+            {
+                erro_de_input_maximo_menor_que_limite_minimo = true;
+            }
+            if (long_maximo > Int32.MaxValue)
+            {
+                erro_de_input_maximo_maior_que_limite_maximo = true;
+            }
         }
 
-        if (long_maximo > Int32.MaxValue)
-        {
-            erro_de_input_maximo_maior_que_limite_maximo = true;
-        }
-
-        if (!(erro_de_input_vazio_minimo || erro_de_input_minimo_menor_que_limite_minimo))
+        if (!(erro_de_input_vazio_minimo ||
+              erro_de_input_minimo_menor_que_limite_minimo || erro_de_input_minimo_maior_que_limite_maximo))
         {
             try { Int32.Parse(tempo_minimo); }
             catch (FormatException fe) { erro_de_input_errado_minimo = true; }
         }
 
-        if (!(erro_de_input_vazio_maximo || erro_de_input_maximo_maior_que_limite_maximo))
+        if (!(erro_de_input_vazio_maximo ||
+              erro_de_input_maximo_menor_que_limite_minimo || erro_de_input_maximo_maior_que_limite_maximo))
         {
             try { Int32.Parse(tempo_maximo); }
             catch (FormatException fe) { erro_de_input_errado_maximo = true; }
@@ -80,13 +96,55 @@ public class GuiVisualizarPorTempo : GuiPadrao2 {
 
         if (!(erro_de_input_errado_minimo || erro_de_input_errado_maximo ||
               erro_de_input_vazio_minimo || erro_de_input_vazio_maximo || 
-              erro_de_input_minimo_menor_que_limite_minimo || erro_de_input_maximo_maior_que_limite_maximo))
+              erro_de_input_minimo_menor_que_limite_minimo || erro_de_input_minimo_maior_que_limite_maximo ||
+              erro_de_input_maximo_menor_que_limite_minimo || erro_de_input_maximo_maior_que_limite_maximo))
         {
             if (Int32.Parse(tempo_minimo) > Int32.Parse(tempo_maximo))
             {
                 erro_de_maior_que = true;
             }
             else { erro_de_maior_que = false; }
+        }
+    }
+
+    void PossiveisMensagensDeErro()
+    {
+        if (erro_de_input_errado_minimo)
+        {
+            GUI.Label(new Rect(0, posicao_erro_y, 210, 40), "Valor mínimo precisa ser de\n" + "apenas números.", "textfield");
+            posicao_erro_y += 40;
+        }
+        if (erro_de_input_errado_maximo)
+        {
+            GUI.Label(new Rect(0, posicao_erro_y, 210, 40), "Valor máximo precisa ser de\n" + "apenas números.", "textfield");
+            posicao_erro_y += 40;
+        }
+        if (erro_de_input_vazio_minimo)
+        {
+            GUI.Label(new Rect(0, posicao_erro_y, 210, 40), "Falta preencher o valor mínimo.", "textfield");
+            posicao_erro_y += 40;
+        }
+        if (erro_de_input_vazio_maximo)
+        {
+            GUI.Label(new Rect(0, posicao_erro_y, 210, 40), "Falta preencher o valor máximo.", "textfield");
+            posicao_erro_y += 40;
+        }
+        if (erro_de_input_minimo_menor_que_limite_minimo)
+        {
+            // Sim, eu me dou o direito de apontar isso, porquê quem vai colocar o tempo ABSURDAMENTE
+            // menor que zero?!
+            GUI.Label(new Rect(0, posicao_erro_y, 210, 40), "... por favor. Você fez isso de propósito.", "textfield");
+            posicao_erro_y += 40;
+        }
+        if (erro_de_input_maximo_maior_que_limite_maximo)
+        {
+
+            GUI.Label(new Rect(0, posicao_erro_y, 210, 40), "...tempo máximo não pode ultrapassar 2147483647.", "textfield");
+            posicao_erro_y += 40;
+        }
+        if (erro_de_maior_que)
+        {
+            GUI.Label(new Rect(0, posicao_erro_y, 210, 40), "Valor mínimo tem que ser\n" + "menor que o máximo.", "textfield");
         }
     }
 
@@ -107,7 +165,10 @@ public class GuiVisualizarPorTempo : GuiPadrao2 {
 
                 // Deus abençoe que C# me permite fazer essas comparações em sequência.
                 if (!(erro_de_input_errado_minimo || erro_de_input_errado_maximo || erro_de_maior_que ||
-                      erro_de_input_vazio_minimo || erro_de_input_vazio_maximo))
+                      erro_de_input_vazio_minimo || erro_de_input_vazio_maximo || 
+                      erro_de_input_minimo_menor_que_limite_minimo || erro_de_input_minimo_maior_que_limite_maximo ||
+                      erro_de_input_maximo_menor_que_limite_minimo || erro_de_input_maximo_maior_que_limite_maximo
+                      ))
                 {
                     if (visivel_ou_invisivel == 1)
                     {
@@ -123,43 +184,8 @@ public class GuiVisualizarPorTempo : GuiPadrao2 {
                     }
                 }
             }
-            if (erro_de_input_errado_minimo)
-            {
-                GUI.Label(new Rect(0, posicao_erro_y, 210, 40), "Valor mínimo precisa ser de\n" + "apenas números.", "textfield");
-                posicao_erro_y += 40;
-            }
-            if (erro_de_input_errado_maximo)
-            {
-                GUI.Label(new Rect(0, posicao_erro_y, 210, 40), "Valor máximo precisa ser de\n" + "apenas números.", "textfield");
-                posicao_erro_y += 40;
-            }
-            if (erro_de_input_vazio_minimo)
-            {
-                GUI.Label(new Rect(0, posicao_erro_y, 210, 40), "Falta preencher o valor mínimo.", "textfield");
-                posicao_erro_y += 40;
-            }
-            if (erro_de_input_vazio_maximo)
-            {
-                GUI.Label(new Rect(0, posicao_erro_y, 210, 40), "Falta preencher o valor máximo.", "textfield");
-                posicao_erro_y += 40;
-            }
-            if (erro_de_input_minimo_menor_que_limite_minimo)
-            {
-                // Sim, eu me dou o direito de apontar isso, porquê quem vai colocar o tempo ABSURDAMENTE
-                // menor que zero?!
-                GUI.Label(new Rect(0, posicao_erro_y, 210, 40), "... por favor. Você fez isso de propósito.", "textfield");
-                posicao_erro_y += 40;
-            }
-            if (erro_de_input_maximo_maior_que_limite_maximo)
-            {
 
-                GUI.Label(new Rect(0, posicao_erro_y, 210, 40), "...tempo máximo não pode ultrapassar 2147483647.", "textfield");
-                posicao_erro_y += 40;
-            }
-            if (erro_de_maior_que)
-            {
-                GUI.Label(new Rect(0, posicao_erro_y, 210, 40), "Valor mínimo tem que ser\n" + "menor que o máximo.", "textfield");
-            }
+            PossiveisMensagensDeErro();
 
             GUI.EndGroup();
         }
