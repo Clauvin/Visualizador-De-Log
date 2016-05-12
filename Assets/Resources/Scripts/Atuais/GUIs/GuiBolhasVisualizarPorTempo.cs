d3f8a10da59efc454c951c0463b37e74725a826f
Clauvin <clauvin_almeida@hotmail.com>
2016-05-12 13:54:58 -0300

    Dividindo GuiVisualizarPorTempo para FIT e Bolhas.

---
+++ Assets/Resources/Scripts/Atuais/GUIs/GuiBolhasVisualizarPorTempo.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Classe GuiBolhasVisualizarPorTempo. Derivada de GuiVisualizarPorTempo e vazia.
+/// Essencialmente, existe para manter o padrão e por possível necessidade futura de alterar alguma coisa.
+/// </summary>
+public class GuiBolhasVisualizarPorTempo : GuiVisualizarPorTempo
+{
+
+}
---
+++ Assets/Resources/Scripts/Atuais/GUIs/GuiFITVisualizarPorTempo.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Classe GuiFITVisualizarPorTempo. Derivada de GuiVisualizarPorTempo e vazia.
+/// Essencialmente, existe para manter o padrão e por possível necessidade futura de alterar alguma coisa.
+/// </summary>
+public class GuiFITVisualizarPorTempo : GuiVisualizarPorTempo
+{
+
+}
