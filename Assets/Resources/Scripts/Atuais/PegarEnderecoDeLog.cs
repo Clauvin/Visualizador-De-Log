23b225c74e0749157b1d751fe5c0687c7bf396d1
Clauvin <clauvin_almeida@hotmail.com>
2016-05-19 15:31:31 -0300

    Fim do Preenchimento de PegarEnderecoDeLog

--- Assets/Resources/Scripts/Atuais/PegarEnderecoDeLog.cs
+++ Assets/Resources/Scripts/Atuais/PegarEnderecoDeLog.cs
@@ -9,7 +9,7 @@ using System.IO;
 /// </summary>
 public class PegarEnderecoDeLog : MonoBehaviour {
 
-    string endereco_de_arquivo;
+    public string endereco_de_arquivo;
 
     private bool Checagem(string endereco)
     {
@@ -31,6 +31,38 @@ public class PegarEnderecoDeLog : MonoBehaviour {
         endereco_de_arquivo = EditorUtility.OpenFilePanel("Teste", CarregarEnderecoDeUltimaPaginaChecada(), "txt");
 
         return Checagem(endereco_de_arquivo);
+    }
+
+    //MELHORAR: No futuro, que hajam dois arquivos. Um para a posição do log do F!T e outro para a posição do log do Bolhas
+    public void CriarIniDeUltimaPaginaChecada(string enderecotodo)
+    {
+        // Handle any problems that might arise when reading the text
+
+        string posicaodoarquivo = Path.GetFullPath("local.ini");
+        FileStream fs;
+        StreamWriter sw;
+
+        string[] enderecoseparado;
+        string enderecomodificado = "";
+
+        enderecoseparado = enderecotodo.Split('/');
+        for (int i = 0; i < enderecoseparado.GetUpperBound(0); i++)
+        {
+            enderecomodificado += enderecoseparado[i];
+            enderecomodificado += "/";
+        }
+
+        // Create a new StreamReader, tell it which file to read and what encoding the file
+        // was saved as
+        fs = File.Create(posicaodoarquivo);
+        sw = new StreamWriter(fs);
+        sw.Write(enderecomodificado);
+
+        sw.Dispose();
+        sw.Close();
+        fs.Dispose();
+        fs.Close();
+
     }
 
     public string CarregarEnderecoDeUltimaPaginaChecada()
