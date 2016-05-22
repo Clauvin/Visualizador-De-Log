dfe3a3d03d61e27435df6b1bbcad01b95b31e0d3
Clauvin <clauvin_almeida@hotmail.com>
2016-05-21 23:27:05 -0300

    Criação De Classe PassadorDeDados

---
+++ Assets/Resources/Scripts/Atuais/PassadorDeDados.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Classe PassadorDeDados. Responsável por passar valores do pre-load para o load.
+/// <para>Isso é necessário porquÊ o pre-load e o load são ambos scenes diferentes, e antes de uma scene ser carregada,
+/// tudo da scene anterior é apagado... a menos que você queira que algo não seja.</para>
+/// <para>E eu prefiro passa ro mínimo de dados possíveis de uma scene para a outra, por isso essa classe específica,
+/// e não uma já existente.</para>
+/// </summary>
+public class PassadorDeDados : MonoBehaviour {
+
+    public int tempo_minimo;
+    public int tempo_maximo;
+    public string endereco_do_arquivo;
+
+    public void SetValuesDePassagem(int tempo_min, int tempo_max, string endereco)
+    {
+        tempo_minimo = tempo_min;
+        tempo_maximo = tempo_max;
+        endereco_do_arquivo = endereco;
+    }
+
+    public void Destruir()
+    {
+        Destroy(transform.gameObject);
+    }
+
+    void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
