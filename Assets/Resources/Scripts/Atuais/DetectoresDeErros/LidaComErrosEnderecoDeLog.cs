5b337dbbe94ef6ad9d4d2983a229efbf6705aae2
Clauvin <clauvin_almeida@hotmail.com>
2016-05-23 12:18:56 -0300

    Criação em Progresso de LidaComErrosEnderecoDeLog

--- Assets/Resources/Scripts/Atuais/DetectoresDeErros/LidaComErrosEnderecoDeLog.cs
+++ Assets/Resources/Scripts/Atuais/DetectoresDeErros/LidaComErrosEnderecoDeLog.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.IO;
 
 /// <summary>
 /// Classe LidaComErrosTempoMinimoEMaximo. Criada para detectar erros no arquivo escolhido para ser carregado pelo visualizador de logs,
@@ -8,133 +9,103 @@ using System.Collections;
 public class LidaComErrosEnderecoDeLog : MonoBehaviour
 {
 
+    // variáveis que definem se houve ou não erro, o que mostra mensagens de erro.
+    public bool erro_de_inexistencia_do_arquivo;
+    public bool erro_de_caminho_escolhido_invalido;
+    public bool erro_de_caminho_escolhido_longo_demais;
+    public bool erro_de_extensao_nao_txt;
+    public bool erro_de_log_errado;
+
+    // considerando que essa classe vai ser usada tanto pra logs do FIT quanto do Bolhas, é necessária esta variável para comparar
+    // com a primeira linha, se ela é [Mode FIT] ou [Mode Bolhas]
+    public string valor_de_comparacao_de_tipo_de_log;
+
+    public int x_da_janela_de_erro;
     public int posicao_da_mensagem_de_erro_y;
-
-    //variáveis que definem se houve ou não erro, o que mostra mensagens de erro.
-    public bool erro_de_input_errado_minimo = false;
-    public bool erro_de_input_errado_maximo = false;
-    public bool erro_de_input_vazio_minimo = false;
-    public bool erro_de_input_vazio_maximo = false;
-
-    // nesse caso limite minimo é o Int32.MinValue, já que a função de deixar visível ou invisível tem como input dois ints.
-    // essa detecção poderia ser feita na função de deixar visível ou invisível, mas
-    // é melhor deixar as deteccoes e tratamentos de input errado numa função só pra isso
-    // do que espalhar elas pelo código.
-    public bool erro_de_input_minimo_menor_que_limite_minimo = false;
-    public bool erro_de_input_minimo_maior_que_limite_maximo = false;
-    public bool erro_de_input_maximo_menor_que_limite_minimo = false;
-    public bool erro_de_input_maximo_maior_que_limite_maximo = false;
-    public bool erro_de_maior_que = false;
-
-    public int x_da_janela_de_erro_de_erro_minimo;
-    public int largura_da_janela_de_erro_de_erro_minimo;
-    public int altura_da_janela_de_erro_de_erro_minimo;
-
-    public int x_da_janela_de_erro_de_erro_maximo;
-    public int largura_da_janela_de_erro_de_erro_maximo;
-    public int altura_da_janela_de_erro_de_erro_maximo;
-
+    public int largura_da_janela_de_erro;
+    public int altura_da_janela_de_erro;
     public int quantidade_de_mudanca_de_posicao_y;
 
+    // Variáveis necessárias para tentar abrir o log através do endereco dado e ver se existem erros.
+    FileStream filestream_de_teste;
+    StreamReader streamreader_de_teste;
 
-    public void ConfigurarVariaveisParaVisualizacaoDeObjetos()
+    public void ConfigurarVariaveis()
     {
 
-        x_da_janela_de_erro_de_erro_minimo = 0;
-        largura_da_janela_de_erro_de_erro_minimo = 210;
-        altura_da_janela_de_erro_de_erro_minimo = 40;
-
-        x_da_janela_de_erro_de_erro_maximo = 0;
-        largura_da_janela_de_erro_de_erro_maximo = 210;
-        altura_da_janela_de_erro_de_erro_maximo = 40;
-
-        posicao_da_mensagem_de_erro_y = 100;
-        quantidade_de_mudanca_de_posicao_y = 40;
+        /*x_da_janela_de_erro = ;
+        posicao_da_mensagem_de_erro_y = ;
+        largura_da_janela_de_erro = ;
+        altura_da_janela_de_erro = ;
+        quantidade_de_mudanca_de_posicao_y = ;*/
 
     }
 
-    public void ConfigurarVariaveisParaPreload()
+    private bool AExtensaoETxt(string endereco)
     {
-        x_da_janela_de_erro_de_erro_minimo = Screen.width / 4;
-        largura_da_janela_de_erro_de_erro_minimo = 240;
-        altura_da_janela_de_erro_de_erro_minimo = 40;
+        string[] checagem;
+        string extensao;
 
-        x_da_janela_de_erro_de_erro_maximo = Screen.width / 2;
-        largura_da_janela_de_erro_de_erro_maximo = 240;
-        altura_da_janela_de_erro_de_erro_maximo = 40;
+        checagem = endereco.Split('/');
+        if (checagem[0] == string.Empty) return false;
+        extensao = checagem[checagem.GetUpperBound(0)].Split('.')[1];
 
-        posicao_da_mensagem_de_erro_y = Screen.height / 2 + 60;
-        quantidade_de_mudanca_de_posicao_y = 0;
+        if (extensao == "txt") return true;
+        else return false;
     }
 
-    public void DetectarETratarErrosEExcecoesDeInput(string tempo_minimo, string tempo_maximo)
+    public void DetectarETratarErrosEExcecoesDeInput(string endereco)
     {
         // esse reset das variáveis ao usar essa função é necessário para evitar
-        // que as mensagens de inputs errados bloqueiem as mensagens de inputs vazios.
-        erro_de_input_vazio_minimo = false;
-        erro_de_input_vazio_maximo = false;
-        erro_de_input_errado_minimo = false;
-        erro_de_input_errado_maximo = false;
-        erro_de_input_minimo_menor_que_limite_minimo = false;
-        erro_de_input_minimo_maior_que_limite_maximo = false;
-        erro_de_input_maximo_menor_que_limite_minimo = false;
-        erro_de_input_maximo_maior_que_limite_maximo = false;
-        erro_de_maior_que = false;
+        // que mensagens bloqueiem umas as outras.
+        erro_de_inexistencia_do_arquivo = false;
+        erro_de_caminho_escolhido_invalido = false;
+        erro_de_caminho_escolhido_longo_demais = false;
+        erro_de_extensao_nao_txt = false;
+        erro_de_log_errado = false;
 
-        if (tempo_minimo == "") { erro_de_input_vazio_minimo = true; }
+        if (endereco == "") { erro_de_caminho_escolhido_invalido = true; }
 
-        if (tempo_maximo == "") { erro_de_input_vazio_maximo = true; }
-
-        // teste de valor mínimo: está alem do limite minimo e máximo que o int comporta?
-        if (!erro_de_input_vazio_minimo)
+        // aqui fazemos um teste de abertura de arquivo. As exceptions encontradas são tratadas, setando as variáveis
+        // adequadas como true.
+        try {
+            filestream_de_teste = new FileStream(endereco, FileMode.Open);
+        }
+        // O arquivo escolhido existe?
+        catch (FileNotFoundException ex)
         {
-            try { Int32.Parse(tempo_minimo); }
-            catch (FormatException fe) { erro_de_input_errado_minimo = true; }
-            catch (OverflowException oe)
+            erro_de_inexistencia_do_arquivo = true;
+        }
+        // O caminho escolhido é inválido?
+        catch (DirectoryNotFoundException dn)
+        {
+            erro_de_caminho_escolhido_invalido = true;
+        }
+        // O caminho escolhido é longo demais?
+        catch (PathTooLongException pt)
+        {
+            erro_de_caminho_escolhido_longo_demais = true;
+        }
+
+        // Se não foram encontrados erros até agora...
+        if ((erro_de_inexistencia_do_arquivo) && (erro_de_caminho_escolhido_invalido) && (erro_de_caminho_escolhido_longo_demais)){
+        // O arquivo escolhido não tem extensão txt?
+            if (!AExtensaoETxt(endereco))
             {
-                long long_minimo = long.Parse(tempo_minimo, NumberStyles.AllowThousands | NumberStyles.AllowLeadingSign);
-                if (long_minimo < Int32.MinValue)
+                erro_de_extensao_nao_txt = true;
+            }
+        // Se tem extensão txt, é o arquivo correto para o que se pretende carregar?
+            else
+            {
+                streamreader_de_teste = new StreamReader(filestream_de_teste);
+                if (streamreader_de_teste.ReadLine() != valor_de_comparacao_de_tipo_de_log)
                 {
-                    erro_de_input_minimo_menor_que_limite_minimo = true;
-                }
-                else if (long_minimo > Int32.MaxValue)
-                {
-                    erro_de_input_minimo_maior_que_limite_maximo = true;
+                    erro_de_log_errado = true;
                 }
             }
         }
-
-        // teste de valor máximo: está alem do limite minimo e máximo que o int comporta?
-        if (!erro_de_input_vazio_maximo)
-        {
-            try { Int32.Parse(tempo_maximo); }
-            catch (FormatException fe) { erro_de_input_errado_maximo = true; }
-            catch (OverflowException oe)
-            {
-                long long_maximo = long.Parse(tempo_maximo, NumberStyles.AllowThousands | NumberStyles.AllowLeadingSign);
-
-                if (long_maximo < Int32.MinValue)
-                {
-                    erro_de_input_maximo_menor_que_limite_minimo = true;
-                }
-                else if (long_maximo > Int32.MaxValue)
-                {
-                    erro_de_input_maximo_maior_que_limite_maximo = true;
-                }
-            }
-        }
-
-        if (!(erro_de_input_errado_minimo || erro_de_input_errado_maximo ||
-              erro_de_input_vazio_minimo || erro_de_input_vazio_maximo ||
-              erro_de_input_minimo_menor_que_limite_minimo || erro_de_input_minimo_maior_que_limite_maximo ||
-              erro_de_input_maximo_menor_que_limite_minimo || erro_de_input_maximo_maior_que_limite_maximo))
-        {
-            if (Int32.Parse(tempo_minimo) > Int32.Parse(tempo_maximo))
-            {
-                erro_de_maior_que = true;
-            }
-            else { erro_de_maior_que = false; }
-        }
+        filestream_de_teste.Dispose();
+        filestream_de_teste.Close();
     }
 
     public void PossiveisMensagensDeErro()
@@ -212,14 +183,11 @@ public class LidaComErrosEnderecoDeLog : MonoBehaviour
 
     public bool NaoTemosErrosDeInput()
     {
-
         // Deus abençoe que C# me permite fazer essas comparações em sequência.
-        return !(erro_de_input_errado_minimo || erro_de_input_errado_maximo || erro_de_maior_que ||
-                      erro_de_input_vazio_minimo || erro_de_input_vazio_maximo ||
-                      erro_de_input_minimo_menor_que_limite_minimo || erro_de_input_minimo_maior_que_limite_maximo ||
-                      erro_de_input_maximo_menor_que_limite_minimo || erro_de_input_maximo_maior_que_limite_maximo
+        return !(erro_de_inexistencia_do_arquivo || erro_de_caminho_escolhido_invalido ||
+                      erro_de_caminho_escolhido_longo_demais || erro_de_extensao_nao_txt ||
+                      erro_de_log_errado
                       );
-
     }
 
     // Use this for initialization
