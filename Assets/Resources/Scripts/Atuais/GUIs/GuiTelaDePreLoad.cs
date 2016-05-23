43924e42766b8e21c1fd14f7a84604a5df2ca3ec
Clauvin <clauvin_almeida@hotmail.com>
2016-05-23 16:44:48 -0300

    Fim do Desmembramento do PreLoad em Pai e Filhos

---
+++ Assets/Resources/Scripts/Atuais/GUIs/GuiTelaDePreLoad.cs
@@ -0,0 +1,123 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System;
+
+public class GuiTelaDePreLoad : GuiPadrao2 {
+
+    protected GUIStyle estilo_titulo_tela_de_preload;
+    protected GUIStyle estilo_botoes_tela_de_preload;
+    protected int posicaox;
+    protected int qualbotao = -1;
+    protected int resultado = -1;
+    protected string[] toolbarStrings = { "Escolher Log", "Visualizar Log",
+                                        "Retornar Para Tela Título" };
+    protected string tempo_minimo = "Apenas números >= a 0 aqui.";
+    protected string tempo_maximo = "Apenas números >= a 0 aqui.";
+
+    protected bool creditos = false;
+
+    protected PegarEnderecoDeLog pegar_endereco_do_log;
+    protected string endereco = "Aqui ficará o endereço do log.";
+    protected string nome_do_arquivo = "Nome do Arquivo de Log";
+    protected LidaComErrosTempoMinimoEMaximo lida_com_erros_min_e_max;
+    protected LidaComErrosEnderecoDeLog lida_com_erros_endereco_de_log;
+
+    // todas as variáveis abaixo são para a leitura do tempo mínimo e máximo do log.
+    protected FileStream fs;
+    protected StreamReader theReader;
+    protected string line;
+    protected string control_line;
+    protected string[] entries;
+
+    protected string titulo = "Escolha de Log do FIT\n" + "e Tempo Carregado do Log";
+
+    public override void OnGUI()
+    {
+        lida_com_erros_min_e_max.ConfigurarVariaveisParaPreload();
+        lida_com_erros_endereco_de_log.ConfigurarVariaveis();
+        GUI.BeginGroup(new Rect(0, 0, Screen.width, Screen.height));
+
+        posicaox = 0;
+        GUI.Box(new Rect(0, 0, Screen.width, Screen.height), string.Empty);
+        GUI.Box(new Rect(Screen.width / 4, Screen.height / 9, Screen.width / 2, Screen.height / 4), titulo, estilo_titulo_tela_de_preload);
+
+        GUI.Label(new Rect(Screen.width / 4, Screen.height / 2 - 20, Screen.width / 2, 20), nome_do_arquivo);
+        GUI.Label(new Rect(Screen.width / 4, Screen.height / 2, Screen.width / 2, 20), endereco, "textfield");
+        GUI.Label(new Rect(Screen.width / 4, Screen.height / 2 + 40, 120, 20), "Tempo Mínimo", "textfield");
+        GUI.Label(new Rect(Screen.width / 2, Screen.height / 2 + 40, 120, 20), "Tempo Máximo", "textfield");
+        tempo_minimo = GUI.TextArea(new Rect(Screen.width / 4, Screen.height / 2 + 60, 240, 20), tempo_minimo);
+        tempo_maximo = GUI.TextArea(new Rect(Screen.width / 2, Screen.height / 2 + 60, 240, 20), tempo_maximo);
+
+        resultado = GUI.Toolbar(new Rect(Screen.width / 12 * 3, Screen.height / 10 * 8, Screen.width / 12 * 6, Screen.height / 10), qualbotao,
+            toolbarStrings);
+
+        switch (resultado)
+        {
+            // Abre a janela do FIT de escolher arquivo, e lê do arquivo escolhido seu tempo inicial e final.
+            case 0:
+
+                EscolhaDeArquivo();
+                break;
+            // Vai para o visualizador do FITs
+            case 1:
+
+                lida_com_erros_min_e_max.DetectarETratarErrosEExcecoesDeInput(tempo_minimo, tempo_maximo);
+                lida_com_erros_endereco_de_log.DetectarETratarErrosEExcecoesDeInput(endereco);
+
+                if (lida_com_erros_min_e_max.NaoTemosErrosDeInput() && lida_com_erros_endereco_de_log.NaoTemosErrosDeInput())
+                {
+                    PassadorDeDados pd = FindObjectOfType<PassadorDeDados>();
+                    pd.SetValuesDePassagem(Convert.ToInt32(tempo_minimo), Convert.ToInt32(tempo_maximo), endereco);
+                    IrParaLoad();
+                }
+                break;
+            // Retorna para a tela título
+            case 2:
+                UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+                break;
+            default:
+                break;
+        }
+
+        lida_com_erros_min_e_max.PossiveisMensagensDeErro();
+        lida_com_erros_endereco_de_log.PossiveisMensagensDeErro();
+
+        resultado = -1;
+
+        GUI.EndGroup();
+
+    }
+
+    protected void InicializacaoGenerica()
+    {
+        estilo_titulo_tela_de_preload = new GUIStyle();
+        estilo_titulo_tela_de_preload.alignment = TextAnchor.MiddleCenter;
+        estilo_titulo_tela_de_preload.font = Font.CreateDynamicFontFromOSFont("Verdana", 30);
+
+        estilo_botoes_tela_de_preload = new GUIStyle("box");
+        estilo_botoes_tela_de_preload.alignment = TextAnchor.MiddleCenter;
+        estilo_botoes_tela_de_preload.font = Font.CreateDynamicFontFromOSFont("Verdana", 10);
+
+        pegar_endereco_do_log = new PegarEnderecoDeLog();
+        lida_com_erros_min_e_max = new LidaComErrosTempoMinimoEMaximo();
+        lida_com_erros_endereco_de_log = new LidaComErrosEnderecoDeLog();
+    }
+
+
+    protected virtual void InicializacaoEspecifica()
+    {
+
+    }
+
+    protected virtual void IrParaLoad()
+    {
+
+    }
+
+    protected virtual void EscolhaDeArquivo()
+    {
+
+    }
+
+}
