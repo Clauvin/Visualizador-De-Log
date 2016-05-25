daddcec06d5d469055ac9f6a00a4d6dd2bde45a6
Clauvin <clauvin_almeida@hotmail.com>
2016-05-25 16:13:00 -0300

    Classe GuiBolhasSairDaVisualizacao
    Responsável pelos dois botões na tela de visualização dos elementos do
    log do Bolhas para voltar para o Pre-Load e voltar para a tela inicial.

---
+++ Assets/Resources/Scripts/Atuais/GUIs/GuiBolhasSairDaVisualizacao.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Classe GuiBolhasSairDaVisualizacao. Filha de GuiSairDaVisualizacao, e responsável pelos dois botões
+/// na tela de visualização dos elementos do log do Bolhas para voltar para o Pre-Load e voltar para a tela inicial.
+/// </summary>
+public class GuiBolhasSairDaVisualizacao : GuiSairDaVisualizacao {
+
+    public override void RetornarParaTelaDePreLoad() { UnityEngine.SceneManagement.SceneManager.LoadScene(2); }
+
+}
