732aa73db54930bbb69a1b50c0e38ca398230492
Clauvin <clauvin_almeida@hotmail.com>
2016-05-25 15:10:44 -0300

    Criação de Classe pai LidaComErros

---
+++ Assets/Resources/Scripts/Atuais/DetectoresDeErros/LidaComErros.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+/// <summary>
+/// Classe LidaComErros. Criada com base incompleta das classes para detectar erros no sistema, e uma vez encontrados,
+/// avisar isso e gerar mensagens de erro adequadas para que apareçam na GUI.
+/// <para>ATENÇÃO: ESTA CLASSE NÃO DEVE SER USADA. Seus filhos, claro, mas ela é incompleta e por si só, não basta
+/// para poder lidar com erros.</para>
+/// </summary>
+public class LidaComErros : MonoBehaviour {
+
+    public int posicao_da_mensagem_de_erro_y;
+    public int quantidade_de_mudanca_de_posicao_y;
+
+    public virtual void PossiveisMensagensDeErro() { }
+
+    public virtual bool NaoTemosErrosDeInput() { return false; }
+
+}
