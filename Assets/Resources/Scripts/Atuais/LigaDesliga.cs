8d86693ada8a2f554833e7e062e51e93ff18fcde
Clauvin <clauvin_almeida@hotmail.com>
2016-06-02 17:15:18 -0300

    Update de LigaDesliga

--- Assets/Resources/Scripts/Atuais/LigaDesliga.cs
+++ Assets/Resources/Scripts/Atuais/LigaDesliga.cs
@@ -1,15 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/// <summary>
+/// Responsável por permitir que objetos do programa sejam ligados e desligados, o que
+/// em teoria, vai economizar CPU e GPU.
+/// </summary>
 public class LigaDesliga : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    private void SetActive(bool ativo)
+    {
+        SetActive(ativo);
+    }
+
+    public bool EstaLigado()
+    {
+        return GetComponent<GameObject>().activeSelf;
+    }
+
+    public bool EstaHieraquicamenteLigado()
+    {
+        return GetComponent<GameObject>().activeInHierarchy;
+    }
+
+    public void Alterna()
+    {
+        if (GetComponent<GameObject>().activeSelf == true) Desligar();
+        else Ligar();
+    }
+
+	public void Ligar()
+    {
+        GetComponent<GameObject>().SetActive(true);
+    }
+
+    public void Desligar()
+    {
+        GetComponent<GameObject>().SetActive(false);
+    }
+
+    
+
 }
