78efcfb436d905309b5304666996e7d43466cea0
Clauvin <clauvin_almeida@hotmail.com>
2016-06-30 16:52:28 -0300

    Mudanças em GuiTempo

--- Assets/Resources/Scripts/Atuais/GUIs/GuiTempo.cs
+++ Assets/Resources/Scripts/Atuais/GUIs/GuiTempo.cs
@@ -43,22 +43,22 @@ public class GuiTempo : GuiPadrao2
     {
         if (revelado)
         {
-            GUI.BeginGroup(new Rect(posx, posy, 320, 200));
+            GUI.BeginGroup(new Rect(posx, posy, 320, 240));
 
             posicao_y = 0;
 
-            GUI.Label(new Rect(10, 0, 210, 40), "Tempo Mostrado em Câmera: " + pos_tempo + " de \n", "textfield");
+            GUI.Label(new Rect(10, 0, 210, 40), "Tempo Mostrado\n" + "em Câmera: " + pos_tempo + " de \n", "textfield");
             if (GetComponent<Controlador>().GetAutoMode()) GUI.TextField(new Rect(10, 40, 210, 20), "Modo Automático ativado");
             GUI.Label(new Rect(10, 60, 210, 20), "Pular para Posição", "textfield");
             string_Para_Editar = GUI.TextField(new Rect(10, 80, 210, 20), string_Para_Editar);
 
-            GUI.Label(new Rect(10, 100, 210, 20), "Começo de Modo Automático Customizado", "textfield");
-            auto_custom_1 = GUI.TextField(new Rect(10, 120, 210, 20), auto_custom_1);
-            GUI.Label(new Rect(10, 140, 210, 20), "Fim de Modo Automático Customizado", "textfield");
-            auto_custom_2 = GUI.TextField(new Rect(10, 160, 210, 20), auto_custom_2);
+            GUI.Label(new Rect(10, 100, 210, 40), "Começo de Modo\nAutomático Customizado", "textfield");
+            auto_custom_1 = GUI.TextField(new Rect(10, 140, 210, 20), auto_custom_1);
+            GUI.Label(new Rect(10, 160, 210, 40), "Fim de Modo\nAutomático Customizado", "textfield");
+            auto_custom_2 = GUI.TextField(new Rect(10, 200, 210, 20), auto_custom_2);
             if (GetComponent<Controlador>().GetAutoMode())
             {
-                GUI.TextField(new Rect(10, 180, 210, 20), "Modo Automático Customizado ativado");
+                GUI.TextField(new Rect(10, 220, 210, 20), "Modo Automático Customizado ativado");
             }
 
             GUI.EndGroup();
