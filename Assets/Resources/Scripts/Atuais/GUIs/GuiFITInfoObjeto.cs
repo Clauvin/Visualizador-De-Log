8527d5d1496a938af86af204030654509757e914
Clauvin <clauvin_almeida@hotmail.com>
2016-07-09 20:00:15 -0300

    Melhora de GuiFITInfoObjeto

--- Assets/Resources/Scripts/Atuais/GUIs/GuiFITInfoObjeto.cs
+++ Assets/Resources/Scripts/Atuais/GUIs/GuiFITInfoObjeto.cs
@@ -9,9 +9,10 @@ public class GuiFITInfoObjeto : GuiInfoObjeto
 {
     public override void OnGUI()
     {
+        //Personagem, X, Y, Tempo, Tempo Do Servidor, Jogador, ID do Jogador, Modo de Jogo e Instante Mostrado Em Câmera.
         if (revelado)
         {
-            GUI.BeginGroup(new Rect(posx, posy, 220, 80));
+            GUI.BeginGroup(new Rect(posx, posy, 220, 160));
             posicaoy = 0;
             GUI.TextField(new Rect(10, posicaoy, 120, 20), "Personagem = " + dadosdoponto.nome_do_objeto);
             posicaoy += 20;
@@ -19,7 +20,16 @@ public class GuiFITInfoObjeto : GuiInfoObjeto
             posicaoy += 20;
             GUI.TextField(new Rect(10, posicaoy, 120, 20), "Y = " + dadosdoponto.y_log / 32);
             posicaoy += 20;
+            GUI.TextField(new Rect(10, posicaoy, 120, 20), "Instante = " + dadosdoponto.instante_em_camera);
+            posicaoy += 20;
             GUI.TextField(new Rect(10, posicaoy, 120, 20), "Tempo = " + dadosdoponto.tempo);
+            posicaoy += 20;
+            GUI.TextField(new Rect(10, posicaoy, 120, 20), "Tempo do Servidor = " + dadosdoponto.tempo_do_servidor);
+            posicaoy += 20;
+            GUI.TextField(new Rect(10, posicaoy, 120, 20), "Jogador = " + dadosdoponto.qual_jogador);
+            posicaoy += 20;
+            GUI.TextField(new Rect(10, posicaoy, 120, 20), "ID do Jogador = " + dadosdoponto.id_do_jogador);
+            
 
             GUI.EndGroup();
         }
