73f3cb1ae8169541669a858a99306cfe044a1d35
Clauvin <clauvin_almeida@hotmail.com>
2016-08-29 15:12:49 -0300

    Criação de Função DesenharTelaDePreLoad

--- Assets/Resources/Scripts/Atuais/GUIs/GuiTelaDePreLoadBolhas.cs
+++ Assets/Resources/Scripts/Atuais/GUIs/GuiTelaDePreLoadBolhas.cs
@@ -75,6 +75,38 @@ public class GuiTelaDePreLoadBolhas : GuiTelaDePreLoad
         toolbarStrings = new string[] { "Escolher Log", "Visualizar Log", "Retornar Para Tela Título" };
     }
 
+    protected override void DesenharTelaDePreLoad()
+    {
+        GUI.BeginGroup(new Rect(0, 0, Screen.width, Screen.height));
+
+        if (!pegar_endereco_do_log.Get_Desenhar_Navegador())
+        {
+
+            posicaox = 0;
+            GUI.Box(new Rect(0, 0, Screen.width, Screen.height), string.Empty);
+            GUI.Box(new Rect(Screen.width / 4, Screen.height / 9, Screen.width / 2, Screen.height / 4), titulo, estilo_titulo_tela_de_preload);
+
+            GUI.Label(new Rect(Screen.width / 4, Screen.height / 2 - 20, Screen.width / 2, 20), nome_do_arquivo);
+            GUI.TextField(new Rect(Screen.width / 4, Screen.height / 2, Screen.width / 2, 20), endereco, "textfield");
+            GUI.Label(new Rect(Screen.width / 4, Screen.height / 2 + 40, 120, 20), "Instante Mínimo", "textfield");
+            GUI.Label(new Rect(Screen.width / 2, Screen.height / 2 + 40, 120, 20), "Instante Máximo", "textfield");
+            tempo_minimo = GUI.TextArea(new Rect(Screen.width / 4, Screen.height / 2 + 60, 240, 20), tempo_minimo);
+            tempo_maximo = GUI.TextArea(new Rect(Screen.width / 2, Screen.height / 2 + 60, 240, 20), tempo_maximo);
+
+            resultado = GUI.Toolbar(new Rect(Screen.width / 12 * 3, Screen.height / 10 * 8, Screen.width / 12 * 6, Screen.height / 10), qualbotao,
+                toolbarStrings);
+        }
+
+        FuncionamentoDosBotoes();
+
+        lida_com_erros_min_e_max.DesenharPossiveisMensagensDeErro();
+        lida_com_erros_endereco_de_log.DesenharPossiveisMensagensDeErro();
+
+        resultado = -1;
+
+        GUI.EndGroup();
+    }
+
     protected override void FuncionamentoDosBotoes()
     {
         switch (resultado)
