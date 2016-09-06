c9ddb327d0fb71804c7f0d031eaf673af3d8dc75
Clauvin <clauvin_almeida@hotmail.com>
2016-09-06 16:25:09 -0300

    Criação de GuiFITEscolhaDeJogadores

---
+++ Assets/Resources/Scripts/Atuais/GUIs/GuiFITEscolhaDeJogadores.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Classe GuiFITEscolhaDeJogadores, derivada de GuiPadrao2.
+/// <para>Responsável por mostrar dados referentes à escolha de jogadores para o cenário.</para>
+/// </summary>
+public class GuiFITEscolhaDeJogadores : GuiPadrao2
+{
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
