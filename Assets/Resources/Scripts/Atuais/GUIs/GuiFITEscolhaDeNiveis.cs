acf462fcb59f217b28fc4eff46e4596ebc7a731c
Clauvin <clauvin_almeida@hotmail.com>
2016-09-06 17:19:18 -0300

    Criação de GuiFITEscolhaDeNiveis

---
+++ Assets/Resources/Scripts/Atuais/GUIs/GuiFITEscolhaDeNiveis.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+/// <summary>
+/// Classe GuiFITEscolhaDeJogadores, derivada de GuiPadrao2.
+/// <para>Responsável por mostrar dados referentes à escolha de jogadores para o cenário.</para>
+/// </summary>
+public class GuiFITEscolhaDeNiveis : GuiPadrao2
+{
+
+    public bool[] toggleNiveis = new bool[8];
+    public int position = 0;
+
+    private Vector2 scrollViewVector = Vector2.zero;
+
+    public override void OnGUI()
+    {
+
+        // Begin the ScrollView
+        scrollViewVector = GUI.BeginScrollView(new Rect(250, 25, 400, 200), scrollViewVector, new Rect(0, 0, 380, 200));
+
+        GUI.Label(new Rect(0, 0, 200, 600), "", "textarea");
+
+        for (int i = 0; i < 4; i++)
+        {
+            toggleNiveis[i] = GUI.Toggle(new Rect(0, position, 100, 30), toggleNiveis[i], "Nivel " + i);
+            position += 20;
+        }
+
+        position = 0;
+
+        for (int i = 4; i < 7; i++)
+        {
+            toggleNiveis[i] = GUI.Toggle(new Rect(200, position, 100, 30), toggleNiveis[i], "Nivel " + i);
+            position += 20;
+        }
+
+        // End the ScrollView
+        GUI.EndScrollView();
+
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
