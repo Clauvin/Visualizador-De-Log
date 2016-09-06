609518164e63b06809a0cf892222eb857c4297b2
Clauvin <clauvin_almeida@hotmail.com>
2016-09-06 17:29:53 -0300

    Criar GuiFITEscolhaDePersonagens

---
+++ Assets/Resources/Scripts/Atuais/GUIs/GuiFITEscolhaDePersonagens.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+/// <summary>
+/// Classe GuiFITEscolhaDePersonagem, derivada de GuiPadrao2.
+/// <para>Responsável por mostrar dados referentes à escolha de jogadores para o cenário.</para>
+/// </summary>
+public class GuiFITEscolhaDePersonagens : GuiPadrao2
+{
+
+    public bool[] togglePersonagens = new bool[4];
+    public int position = 0;
+
+    private Vector2 scrollViewVector = Vector2.zero;
+
+    public override void OnGUI()
+    {
+
+        // Begin the ScrollView
+        scrollViewVector = GUI.BeginScrollView(new Rect(450, 25, 400, 200), scrollViewVector, new Rect(0, 0, 380, 200));
+
+        GUI.Label(new Rect(0, 0, 200, 600), "", "textarea");
+
+        position = 0;
+
+        for (int i = 0; i < 2; i++)
+        {
+            togglePersonagens[i] = GUI.Toggle(new Rect(0, position, 100, 30), togglePersonagens[i], "Personagem " + i);
+            position += 20;
+        }
+
+        position = 0;
+
+        for (int i = 2; i < 4; i++)
+        {
+            togglePersonagens[i] = GUI.Toggle(new Rect(100, position, 100, 30), togglePersonagens[i], "Personagem " + i);
+            position += 20;
+        }
+
+        // End the ScrollView
+        GUI.EndScrollView();
+
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
