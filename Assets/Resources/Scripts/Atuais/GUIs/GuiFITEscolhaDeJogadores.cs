c5366dda8881d4860ab8f7d74cabfb275f2a0db2
Clauvin <clauvin_almeida@hotmail.com>
2016-09-06 17:05:24 -0300

    Teste de Visual de GuiFITEscolhaDeJogadores

--- Assets/Resources/Scripts/Atuais/GUIs/GuiFITEscolhaDeJogadores.cs
+++ Assets/Resources/Scripts/Atuais/GUIs/GuiFITEscolhaDeJogadores.cs
@@ -11,10 +11,20 @@ public class GuiFITEscolhaDeJogadores : GuiPadrao2
     public bool toggleBool1 = true;
     public bool toggleBool2 = true;
 
+    private Vector2 scrollViewVector = Vector2.zero;
+
     public override void OnGUI() {
 
-        toggleBool1 = GUI.Toggle(new Rect(25, 25, 100, 30), toggleBool, "Toggle");
-        toggleBool2 = GUI.Toggle(new Rect(25, 55, 100, 30), toggleBool, "Toggle");
+        // Begin the ScrollView
+        scrollViewVector = GUI.BeginScrollView(new Rect(25, 25, 200, 300), scrollViewVector, new Rect(0, 0, 180, 600));
+
+        GUI.Label(new Rect(0, 0, 200, 600), "", "textarea");
+
+        toggleBool1 = GUI.Toggle(new Rect(0, 0, 100, 30), toggleBool1, "João");
+        toggleBool2 = GUI.Toggle(new Rect(0, 30, 100, 30), toggleBool2, "José");
+
+        // End the ScrollView
+        GUI.EndScrollView();
 
     }
 
