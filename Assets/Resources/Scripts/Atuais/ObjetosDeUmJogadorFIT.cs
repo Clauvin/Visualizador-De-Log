f40fd959882980c3adb927c3ad821f4e46f74d6b
Clauvin <clauvin_almeida@hotmail.com>
2016-09-20 16:48:34 -0300

    Criando Classe ObjetosDeUmJogadorFIT

---
+++ Assets/Resources/Scripts/Atuais/ObjetosDeUmJogadorFIT.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using Basicas;
+
+public class ObjetosDeUmJogadorFIT : MonoBehaviour {
+
+    public ArrayList lista_de_backgrounds;
+    public ArrayList lista_de_objetos;
+    public ArrayList matrizes_dos_heatmaps;
+    public BancoDeDadosFIT bd_jogador_fit;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
