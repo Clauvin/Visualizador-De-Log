3628e6d18369dde0a49c3a96d5214bbd323d508b
Clauvin <clauvin_almeida@hotmail.com>
2016-09-20 15:33:11 -0300

    NovoLeitorFIT funciona com mudanças anteriores

--- Assets/Resources/Scripts/Atuais/NovoLeitorFIT.cs
+++ Assets/Resources/Scripts/Atuais/NovoLeitorFIT.cs
@@ -22,23 +22,19 @@ public class NovoLeitorFIT : NovoLeitor2 {
 #endif
 
         PassadorDeDados pd = FindObjectOfType<PassadorDeDados>();
-        if (pd.endereco_do_arquivo != "")
-        {
-            pegar_endereco_de_log.endereco_de_arquivo[0] = pd.endereco_do_arquivo;
 
-            // Gambiarra temporária para testar o carregar do arquivo de mapas.
-            pegar_endereco_de_log.endereco_de_arquivo.Add(endereco + "/InfoInicialDasFases.txt");
+        pegar_endereco_de_log.endereco_de_arquivo[0] = pd.endereco_do_arquivo;
 
-            pegar_endereco_de_log.CriarIniDeUltimoLogChecado(pd.endereco_do_arquivo);
-            StartFIT();
-            LoadStuffFIT(pd.tempo_minimo, pd.tempo_maximo);
-            CreateStuffFIT();
-            GetComponent<Controlador>().InicializacaoFIT();
-            pd.Destruir();
-        } else
-        {
-            RetornarParaTelaInicial();
-        }
+        StartFIT();
+        bd_fit = pd.bd_fit;
+        Heatmaps = pd.heatmaps;
+        InicializarHeatmaps();
+
+        CreateStuffFIT();
+        GetComponent<Controlador>().InicializacaoFIT();
+        
+        pd.Destruir();
+
     }
 
 }
