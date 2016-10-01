b50bf0530ef01705190e27ca26bcae1cbf90582f
Clauvin <clauvin_almeida@hotmail.com>
2016-10-01 18:23:39 -0300

    Ativação e desativação de âncoras funcionando
    Ativação e desativação de âncoras funcionando.
    
    Sim, eu sei que esse é um commit ruim, foi a pressa. :/

--- Assets/Resources/Scripts/Atuais/GUIs/GuiFITSelecaoDeJogadores.cs
+++ Assets/Resources/Scripts/Atuais/GUIs/GuiFITSelecaoDeJogadores.cs
@@ -7,6 +7,9 @@ public class GuiFITSelecaoDeJogadores : GuiPadrao2
 {
 
     GUIStyle estilo;
+    bool jogador_1 = false;
+    bool jogador_1_2 = false;
+    bool jogador_2 = false;
 
     public override void OnGUI()
     {
@@ -16,25 +19,54 @@ public class GuiFITSelecaoDeJogadores : GuiPadrao2
 
         GUI.Label(new Rect(10, 10, 180, 20), "Visualização de");
 
-        GUI.Button(new Rect(10, 30, 180, 20), GetComponent<NovoLeitor2>().
+        jogador_1 = GUI.Button(new Rect(10, 30, 180, 20), GetComponent<NovoLeitor2>().
                                                objs_jogadores_fit.GetObjetosDeUmJogadorFIT(0).bd_fit.GetNomeDoJogador(0));
 
         if (GetComponent<NovoLeitor2>().objs_jogadores_fit.obj_jogadores_fit.Count > 1)
         {
 
-            GUI.Button(new Rect(10, 50, 180, 20), GetComponent<NovoLeitor2>().
+            jogador_1_2 = GUI.Button(new Rect(10, 50, 180, 20), GetComponent<NovoLeitor2>().
                                                objs_jogadores_fit.GetObjetosDeUmJogadorFIT(0).bd_fit.GetNomeDoJogador(0) +
                                                " e " +
                                                GetComponent<NovoLeitor2>().
                                                objs_jogadores_fit.GetObjetosDeUmJogadorFIT(1).bd_fit.GetNomeDoJogador(0));
 
-            GUI.Button(new Rect(10, 70, 180, 20), GetComponent<NovoLeitor2>().
+            jogador_2 = GUI.Button(new Rect(10, 70, 180, 20), GetComponent<NovoLeitor2>().
                                                objs_jogadores_fit.GetObjetosDeUmJogadorFIT(1).bd_fit.GetNomeDoJogador(0));
 
         }
 
+        if (jogador_1 || jogador_1_2 || jogador_2)
+        {
+            Controle();
+        }
+
+        jogador_1 = false; jogador_1_2 = false; jogador_2 = false;
+
         GUI.EndGroup();
 
+    }
+
+    void Controle()
+    {
+        if (jogador_1)
+        {
+            ObjetosDeUmJogadorFIT objetos = GetComponent<NovoLeitor2>().objs_jogadores_fit.GetObjetosDeUmJogadorFIT(1);
+            if (objetos.ancora_dos_dados != null) objetos.ancora_dos_dados.SetActive(false);
+            GetComponent<NovoLeitor2>().objs_jogadores_fit.GetObjetosDeUmJogadorFIT(0).ancora_dos_dados.SetActive(true);
+            
+        }
+        else if (jogador_1_2)
+        {
+            ObjetosDeUmJogadorFIT objetos = GetComponent<NovoLeitor2>().objs_jogadores_fit.GetObjetosDeUmJogadorFIT(1);
+            if (objetos.ancora_dos_dados != null) objetos.ancora_dos_dados.SetActive(true);
+            GetComponent<NovoLeitor2>().objs_jogadores_fit.GetObjetosDeUmJogadorFIT(0).ancora_dos_dados.SetActive(true);
+        }
+        else if (jogador_2)
+        {
+            ObjetosDeUmJogadorFIT objetos = GetComponent<NovoLeitor2>().objs_jogadores_fit.GetObjetosDeUmJogadorFIT(1);
+            if (objetos.ancora_dos_dados != null) objetos.ancora_dos_dados.SetActive(true);
+        }
     }
 
     // Use this for initialization
