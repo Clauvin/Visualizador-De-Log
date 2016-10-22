d521211e7ffacb87d62d9170b52e9706aabaebc9
Clauvin <clauvin_almeida@hotmail.com>
2016-10-22 20:08:33 -0200

    Correção ortográfica de descrição de classe...
    Correção ortográfica de descrição de classe GuiFITModo.

--- Assets/Resources/Scripts/Atuais/GUIs/GuiFITModo.cs
+++ Assets/Resources/Scripts/Atuais/GUIs/GuiFITModo.cs
@@ -1,6 +1,6 @@
 ﻿/// <summary>
 /// Classe GuiFITModo, responsável por mostrar qual modo de visualização está sendo visto pelo usuário enquanto visualiza o Bolhas.
-/// <para>Não, isso não é um erro. Está como está para seguir o padrão das outras classes de GUI que tem derivadaos e existe para
+/// <para>Não, isso não é um erro. Está como está para seguir o padrão das outras classes de GUI que tem derivados e existe para
 /// caso tenhamos alguma necessidade de diferenciar essa classe do GuiBolhasModo.</para>
 /// </summary>
 /// 
